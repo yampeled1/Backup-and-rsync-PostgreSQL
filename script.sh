36e70db6b431126457409595672f50c4f7d4f331
Yam <yam@linnovate.net>
2019-12-23 17:15:25 +0200

    Modified script, Added delay leeting the container start, added random so the dump wo'nt override each other.

--- script.sh
+++ script.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-docker build . --tag postgress-anyvision:v1.0
-docker run --name anyvision_db -e POSTGRES_PASSWORD=password -d postgress-anyvision:v1.0
-
-# docker exec postgres connect to db and create table missing
-
-docker exec -t anyvision_db pg_dump -c -U postgres | gzip > anyvision_dump_`date +%Y%m%d`.sql.gz
-ls -t db dumps/ | tail -n +11 | xargs rm --
-rsync -avz --delete db_dumps user@x.x.x.x:/destination
+random="$((1 + RANDOM % 99))"
+cat /dev/urandom | tr -dc '0-9' | fold -w 256 | head -n 1 | head --bytes 1
+docker run --name postgress-anyvision -e POSTGRES_PASSWORD=password -d postgres && sleep 5
+docker exec -i postgress-anyvision psql -U postgres -c "create database anyVision"
+docker exec -i postgress-anyvision psql -U postgres -d anyvision -c "CREATE TABLE anyvison_table (id int, some_row text);"
+docker exec -t anyvision_db pg_dump -c -U postgres | gzip > db_dumps/anyvision_dump"$random"_`date +%Y%m%d`.sql.gz
+cd db_dumps && ls -t  | tail -n +11 | xargs rm --
+#rsync -avz --delete db_dumps user@x.x.x.x:/destination
+docker stop postgress-anyvision && docker rm postgress-anyvision
